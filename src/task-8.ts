905fbe849eedcf3a7c04bd021672f5337681a8d1
KhrystynaKolomiiets <kaulitz89996@gmail.com>
2025-07-17 21:30:21 +0200

    correct mistakes

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
-interface IPost {
+interface Post {
   id: number;
   title: string;
   body: string;
 }
-async function fetchPosts() {
-  const response = await axios.get<IPost[]>(
+async function fetchPosts(): Promise<Post[]> {
+  const response = await axios.get<Post[]>(
     "<https://jsonplaceholder.typicode.com/posts>"
   );
   return response.data;
