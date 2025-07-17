71441424b9e9d36fc6a61869d491f7445b56ead3
KhrystynaKolomiiets <kaulitz89996@gmail.com>
2025-07-17 20:05:13 +0200

    Initial commit

---
+++ src/task-2.ts
@@ -0,0 +1,12 @@
+interface IProduct {
+  readonly id: number;
+  title: string;
+  description?: string;
+}
+const product: IProduct = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
